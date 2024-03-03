05bc2a6b86a0cc5113654583566d2d3528f45e50
co2color <819109255@qq.com>
2024-03-03 20:51:28 +0800

    feat

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-// const detective = require('detective')
+/* eslint-disable no-undef */
+const detective = require('detective')
 // const resolve = require('resolve').sync
-// const fs = require('fs')
+const fs = require('fs')
 import { join } from 'path'
 import { fileURLToPath } from 'url'
 
@@ -9,9 +10,22 @@ const config = {
   'output': 'bundle.js'
 }
 
+let ID = 0
+function createModuleObject(filepath) {
+  // 获取文件的 源码
+  const source = fs.readFileSync(filepath, 'utf-8')
+  // 生成文件依赖 AST 
+  const requires = detective(source)
+  const id = ID++
+
+  return { id, filepath, source, requires }
+}
+
+
 function getEntryFilePath() {
   let entryPathName = config.entry ? config.entry : 'index.js'
   let entryPath = join(fileURLToPath(import.meta.url), entryPathName)
   return entryPath
 }
+createModuleObject('')
 getEntryFilePath()
