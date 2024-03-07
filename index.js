3b775917247ee240eb97e062b6b85ce5db036480
co2color <819109255@qq.com>
2024-03-07 20:44:21 +0800

    feat

--- index.js
+++ index.js
@@ -7,35 +7,27 @@ const config = {
   entry: './src/test.js',
   output: 'bundle.js',
 }
-
 let ID = 0
 function createModuleObject(filepath) {
-  // 获取文件的 源码
+  //获取文件的 源码
   const source = fs.readFileSync(filepath, 'utf-8')
-  // 生成文件依赖 AST
+  //通过 detective 这个库 生成文件依赖 AST 
   const requires = detective(source)
   const id = ID++
 
   return { id, filepath, source, requires }
 }
 
-function getEntryFilePath() {
-  const entryPathName = config.entry ? config.entry : 'index.js'
-  const entryPath = path.join(__dirname, entryPathName)
-  // let entryPath = join(fileURLToPath(import.meta.url), entryPathName)
-  return entryPath
-}
-
 function getModules(entry) {
   const rootModule = createModuleObject(entry)
   const modules = [rootModule]
 
-  // Iterate over the modules, even when new
+  // Iterate over the modules, even when new 
   // ones are being added
   for (const module of modules) {
     module.map = {} // Where we will keep the module maps
 
-    module.requires.forEach((dependency) => {
+    module.requires.forEach(dependency => {
       const basedir = path.dirname(module.filepath)
       const dependencyPath = resolve(dependency, { basedir })
 
@@ -75,10 +67,27 @@ function pack(modules) {
   })({ ${modulesSource} })`
 }
 
+
+function getEntryFilePath(){
+    let entryPathName = config.entry? config.entry: 'index.js';
+    let entryPath = path.join(__dirname,entryPathName);
+    return entryPath;
+}
+
 function getOutputFilePath(){
   let outputPathName = config.output? config.output: 'bundle.js';
   let outputPath = path.join(__dirname,'./dist',outputPathName);
   return outputPath;
 }
 
-const strCode = pack(getModules(getEntryFilePath()))
+function output(data){
+    if(!fs.existsSync('./dist')){
+      fs.mkdirSync('./dist');
+    }
+    fs.writeFileSync(getOutputFilePath(),data,{"flag":"w+"});
+}
+
+(function start(){
+  let outputSource = pack(getModules(getEntryFilePath()));
+  output(outputSource);
+})()
